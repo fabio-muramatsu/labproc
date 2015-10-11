a0ff868baa8eae1c672aa58860b45ba40d2bb74b
Fabio <fabio@debian.workgroup>
2015-10-11 18:46:42 -0300

    Atualizado até reconhecimento de usuários

--- run.py
+++ run.py
@@ -6,13 +6,10 @@ import mfrc522.MFRC522 as MFRC522
 import signal
 import json, time
 from display import i2c_lcd
-from util import *
+from util import write_display, read_input
 
 TYPE_ADMIN = 'admin'
 TYPE_USER = 'user'
-
-#mantém o display em escopo global
-lcd = i2c_lcd(0x27,1, 2, 1, 0, 4, 5, 6, 7, 3)
 
 # Capture SIGINT for cleanup when the script is aborted
 def end_read(signal,frame):
@@ -21,45 +18,10 @@ def end_read(signal,frame):
     continue_reading = False
     GPIO.cleanup()
 
-#TODO: Tentar colocar essas funções em util.py, passando lcd como parâmetro
-def write_display(content):
-    '''
-        Escreve o conteúdo no display, efetuando quebras de linha se necessário
-    '''
-    index = 0
-    if len(content) > 16:
-        words = content.split()
-        word_lengths = [len(w) for w in words]
-        for i in xrange(1,len(words)+1):
-            if sum(word_lengths[:i])+i-1 > 16:
-                index = i-1
-                break
-        if index == 0: #Se a primeira palavra tem mais de 16 letras
-            index = 1
-        lcd.clear()
-        lcd.writeString(' '.join(words[:index]))
-        lcd.setPosition(2,0)
-        lcd.writeString(' '.join(words[index:]))
-    else:
-        lcd.clear()
-        lcd.writeString(content)
-
-
-def read_input():
-    '''
-    Lê uma entrada do usuário até que ele digite ENTER
-    '''
-    c = ''
-    s = ''
-    while c != '\r':
-        s += c
-        c = getch()
-        lcd.writeChar('*')
-    return s
-
 if __name__ == "__main__":
 
     #Init display
+    lcd = i2c_lcd(0x27,1, 2, 1, 0, 4, 5, 6, 7, 3)
     lcd.backLightOn()
     lcd.command(lcd.CMD_Display_Control | lcd.OPT_Enable_Display)
 
@@ -75,7 +37,7 @@ if __name__ == "__main__":
     #{tag_id: [password, account type]}
     id_file.close()
 
-    write_display("Aproxime o cartao")
+    write_display(lcd, "Aproxime o cartao")
     # This loop keeps checking for chips. If one is near it will get the UID and authenticate
     while continue_reading:
         # Scan for cards    
@@ -92,40 +54,40 @@ if __name__ == "__main__":
 
             #Se o ID lido consta no banco de dados, solicita a senha
             if uid_str in ids: 
-                write_display("Digite a senha: ")
+                write_display(lcd, "Digite a senha: ")
                 lcd.setPosition(2,0)
-                password = read_input()
+                password = read_input(lcd)
                 #Se a senha digitada corresponde ao cartão, checa o tipo de usuário
                 if password == ids[uid_str][0]:
                     print "{} autenticado".format(ids[uid_str][1])
 
                     #Usuário comum
                     if ids[uid_str][1] == TYPE_USER:
-                        write_display("Autorizado")
+                        write_display(lcd, "Autorizado")
                         #Sinal de abertura
                         time.sleep(1)
 
-                        write_display("Aproxime o cartao")
+                        write_display(lcd, "Aproxime o cartao")
 
                     #Administrador
                     elif ids[uid_str][1] == TYPE_ADMIN:
-                        write_display("admin")
+                        write_display(lcd, "admin")
                         #Cadsastro de novo cartão
 
                         time.sleep(1)
-                        write_display("Aproxime o cartao")
+                        write_display(lcd, "Aproxime o cartao")
 
                 #Se a senha digitada for incorreta, notifica o usuário, e aguarda nova leitura        
                 else:
-                    write_display("Senha incorreta")
+                    write_display(lcd, "Senha incorreta")
                     time.sleep(3)
-                    write_display("Aproxime o cartao")
+                    write_display(lcd, "Aproxime o cartao")
             
             #Se o cartão apresentado não for reconhecido, notifica o usuário e aguarda nova leitura
             else:
-                write_display("Cartao nao cadastrado")
+                write_display(lcd, "Cartao nao cadastrado")
                 time.sleep(3)
-                write_display("Aproxime o cartao")
+                write_display(lcd, "Aproxime o cartao")
 
 
 
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 def getch():
     import termios
     import sys, tty
@@ -11,3 +13,42 @@ def getch():
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
     return _getch()
+
+def write_display(lcd, content):
+    '''
+        Escreve o conteúdo no display, efetuando quebras de linha se necessário
+    '''
+    lcd.command(lcd.CMD_Display_Control)
+    index = 0
+    if len(content) > 16:
+        words = content.split()
+        word_lengths = [len(w) for w in words]
+        for i in xrange(1,len(words)+1):
+            if sum(word_lengths[:i])+i-1 > 16:
+                index = i-1
+                break
+        if index == 0: #Se a primeira palavra tem mais de 16 letras
+            index = 1
+        lcd.clear()
+        lcd.writeString(' '.join(words[:index]))
+        lcd.setPosition(2,0)
+        lcd.writeString(' '.join(words[index:]))
+    else:
+        lcd.clear()
+        lcd.writeString(content)
+
+    lcd.command(lcd.CMD_Display_Control | lcd.OPT_Enable_Display)
+
+
+def read_input(lcd):
+    '''
+    Lê uma entrada do usuário até que ele digite ENTER
+    '''
+    c = ''
+    s = ''
+    while c != '\r':
+        c = getch()
+        if c.isalnum():
+            s += c
+            lcd.writeChar('*')
+    return s
